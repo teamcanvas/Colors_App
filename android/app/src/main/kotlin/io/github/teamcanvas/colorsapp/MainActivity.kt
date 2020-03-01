0b8e0ff63e4bc998f92e7e970b3215fff785a256
방준서 <bbang@coinplug.com>
2020-03-01 20:48:40 +0900

    Initial Commit!

---
+++ android/app/src/main/kotlin/io/github/teamcanvas/colorsapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.teamcanvas.colorsapp
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
